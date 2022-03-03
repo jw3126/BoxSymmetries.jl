7551e374256859b32c9bbdc89e7e6a5e64d9cb27
Jan Weidner <jw3126@gmail.com>
2022-03-03 15:41:30 +0100

    fix argcheck

--- src/BoxSymmetries.jl
+++ src/BoxSymmetries.jl
@@ -11,7 +11,7 @@ Base.ndims(::Type{BoxSymmetry{N}}) where {N} = N
 
 function outaxes(o::BoxSymmetry, axes::Tuple)
     @argcheck length(axes) == ndims(o)
-    check(o)
+    @argcheck ispermutation(o.permutation)
     unsafe_permute(o.permutation, axes)
 end
 
@@ -22,8 +22,8 @@ function unsafe_permute(perm::NTuple{N}, items::NTuple{N,Any}) where {N}
 end
 
 function apply_symmetry!(out::AbstractArray{<:Any, N}, o::BoxSymmetry{N}, x) where {N}
-    check(o)
     @argcheck axes(out) == outaxes(o, axes(x))
+    @argcheck ispermutation(o.permutation)
     flipped_axes = map(flipaxis, axes(x), o.flipsign)
     @inbounds for I in CartesianIndices(x)
         indsx = Tuple(I)
@@ -51,19 +51,14 @@ function flipaxis(ax::AbstractRange, flip::Bool)
     end
 end
 
-function check(o::BoxSymmetry)
-    for key in 1:ndims(o)
-        if !(key in o.permutation)
-            msg = """
-            Invalid permutation, key missing:
-            key = $key
-            symmetry = $o
-            permutation = $(o.permutation)
-            """
-            throw(ArgumentError(msg))
-        end
+function ispermutation(perm::NTuple{N,Int}) where {N}
+    ret = true
+    for key in ntuple(identity, Val(N))
+        ret = ret & (key in perm)
     end
+    ret
 end
+
 """
 
     sym(args::Integer...)
@@ -72,9 +67,9 @@ Create a box symmetry, according to args.
 """
 function sym(args::Integer...)
     perm = map(abs, args)
+    @argcheck ispermutation(perm)
     flip = map(<(0), args)
     ret = BoxSymmetry(perm, flip)
-    check(ret)
     ret
 end
 function astuple(o::BoxSymmetry)
@@ -89,25 +84,23 @@ end
 #### symmetries
 ################################################################################
 
-function symmetries(::Val{1})
-    [sym(1), sym(-1)]
-end
-function symmetries(::Val{2})
-    [
-        sym(1,2), sym(1,-2), sym(-1,2), sym(-1,-2),
-        sym(2,1), sym(2,-1), sym(-2,1), sym(-2,-1),
-    ]
-end
-function symmetries(::Val{3})
-    [
-        sym(1,2,3), sym(1,2,-3), sym(1,-2,3), sym(1,-2,-3), sym(-1,2,3), sym(-1,2,-3), sym(-1,-2,3), sym(-1,-2,-3), 
-        sym(1,3,2), sym(1,3,-2), sym(1,-3,2), sym(1,-3,-2), sym(-1,3,2), sym(-1,3,-2), sym(-1,-3,2), sym(-1,-3,-2), 
-        sym(3,2,1), sym(3,2,-1), sym(3,-2,1), sym(3,-2,-1), sym(-3,2,1), sym(-3,2,-1), sym(-3,-2,1), sym(-3,-2,-1), 
-        sym(2,1,3), sym(2,1,-3), sym(2,-1,3), sym(2,-1,-3), sym(-2,1,3), sym(-2,1,-3), sym(-2,-1,3), sym(-2,-1,-3), 
-        sym(2,3,1), sym(2,3,-1), sym(2,-3,1), sym(2,-3,-1), sym(-2,3,1), sym(-2,3,-1), sym(-2,-3,1), sym(-2,-3,-1), 
-        sym(3,1,2), sym(3,1,-2), sym(3,-1,2), sym(3,-1,-2), sym(-3,1,2), sym(-3,1,-2), sym(-3,-1,2), sym(-3,-1,-2), 
-    ]
-end
+const SYMMETRIES1D = [sym(1), sym(-1)]
+const SYMMETRIES2D = [
+    sym(1,2), sym(1,-2), sym(-1,2), sym(-1,-2),
+    sym(2,1), sym(2,-1), sym(-2,1), sym(-2,-1),
+]
+const SYMMETRIES3D = [
+    sym(1,2,3), sym(1,2,-3), sym(1,-2,3), sym(1,-2,-3), sym(-1,2,3), sym(-1,2,-3), sym(-1,-2,3), sym(-1,-2,-3), 
+    sym(1,3,2), sym(1,3,-2), sym(1,-3,2), sym(1,-3,-2), sym(-1,3,2), sym(-1,3,-2), sym(-1,-3,2), sym(-1,-3,-2), 
+    sym(3,2,1), sym(3,2,-1), sym(3,-2,1), sym(3,-2,-1), sym(-3,2,1), sym(-3,2,-1), sym(-3,-2,1), sym(-3,-2,-1), 
+    sym(2,1,3), sym(2,1,-3), sym(2,-1,3), sym(2,-1,-3), sym(-2,1,3), sym(-2,1,-3), sym(-2,-1,3), sym(-2,-1,-3), 
+    sym(2,3,1), sym(2,3,-1), sym(2,-3,1), sym(2,-3,-1), sym(-2,3,1), sym(-2,3,-1), sym(-2,-3,1), sym(-2,-3,-1), 
+    sym(3,1,2), sym(3,1,-2), sym(3,-1,2), sym(3,-1,-2), sym(-3,1,2), sym(-3,1,-2), sym(-3,-1,2), sym(-3,-1,-2), 
+]
+
+symmetries(::Val{1}) = SYMMETRIES1D
+symmetries(::Val{2}) = SYMMETRIES2D
+symmetries(::Val{3}) = SYMMETRIES3D
 
 """
     symmetries(dim::Integer)
@@ -116,11 +109,12 @@ end
 Return an iterator over all box symmeties of a given dimension.
 """
 function symmetries(n::Integer)
-    symmetries(Val(n))
+    symmetries(Val(Int(n)))
 end
 
 end
 
+# TODO rand
 # TODO composition of symmetries
 # TODO inversion of symmetries
 # TODO lazy iterator of all symmetries of given dimension
