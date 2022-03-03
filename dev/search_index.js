a4d643ed95c11b85c06faa484450b8454d62efb3
Documenter.jl <documenter@juliadocs.github.io>
2022-03-03 12:02:56 +0000

    build based on 8664b98

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoxSymmetries","category":"page"},{"location":"#BoxSymmetries","page":"Home","title":"BoxSymmetries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoxSymmetries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoxSymmetries]","category":"page"}]
+}
